b6d0bb3ce5041e4ca43516dd263431306576e5fb
K-Saikrishnan <53394202+K-Saikrishnan@users.noreply.github.com>
2025-10-12 18:30:13 +0530

    ci(commitlint): remove body-max-line-length rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,11 +2,5 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-empty': [2, 'never'],
-    'references-empty': [2, 'never'],
-  },
-  parserPreset: {
-    parserOpts: {
-      issuePrefixes: ['GH-'],
-    },
   },
 };
