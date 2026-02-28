c9946afd67b235fe30f9e3e6889aee022ece9e96
K-Saikrishnan <53394202+K-Saikrishnan@users.noreply.github.com>
2026-02-28 11:00:36 +0530

    fix(tests): fix pytest coverage config

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,2 +1,12 @@
-def test_truth() -> None:
-  assert True
+from typer.testing import CliRunner
+
+from python_projects.main import app
+
+runner = CliRunner()
+
+
+def test_app() -> None:
+  res = runner.invoke(app)
+
+  assert res.exit_code == 0
+  assert res.output == ''
